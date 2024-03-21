d81b6e657724e932d039604bece349bbdaa81fa8
Tank 228 <92095847+tankoman228@users.noreply.github.com>
2024-03-21 22:33:06 +0700

    Вёрстка форума и материалов

---
+++ app/src/main/java/com/example/learntogether_mobile/ForumAskAdd.java
@@ -0,0 +1,14 @@
+package com.example.learntogether_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ForumAskAdd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forum_ask_add);
+    }
+}
