0d7a113c01caea47bdb609172e1f7356191501a1
tank228 <Admin@DESKTOP-4RLL8V2>
2024-04-11 15:56:36 +0700

    Вёрстка для группы и админского функционала

---
+++ app/src/main/java/com/example/learntogether_mobile/Activities/WatchProfile.java
@@ -0,0 +1,16 @@
+package com.example.learntogether_mobile.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.learntogether_mobile.R;
+
+public class WatchProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_watch_profile);
+    }
+}
